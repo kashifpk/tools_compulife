207f214ab453296fca9a4678143eb6367789ada4
Kashif Iftikhar <kashif@compulife.com.pk>
2014-12-06 20:26:49 +0500

    subapp requirement dependencies implementation

--- setup.py
+++ setup.py
@@ -4,6 +4,7 @@ import sys
 from setuptools import setup, find_packages
 
 here = os.path.abspath(os.path.dirname(__file__))
+
 requires = [
     'PyCK',
     'pyramid',
@@ -16,12 +17,21 @@ requires = [
     'webtest',
     'waitress',
     'wtforms',
-    'wtdojo',
-    'pygments'
+    'wtdojo'
 ]
+
+# Requires from subapps
+from tools_compulife.apps import enabled_apps
+for enabled_app in enabled_apps:
+    if hasattr(enabled_app, 'app_requires'):
+        for requirement in enabled_app.app_requires:
+            if requirement not in requires:
+                requires.append(requirement)
 
 if sys.version_info[:3] < (2, 5, 0):
     requires.append('pysqlite')
+
+print(requires)
 
 setup(
     name='tools_compulife',
