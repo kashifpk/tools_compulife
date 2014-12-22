c2f684c823fceb1fabde6a6d34cf6bd99ba4062d
Kashif Iftikhar <kashif@compulife.com.pk>
2014-12-22 22:47:59 +0500

    Removed commented temp code

--- lib/geoip.py
+++ lib/geoip.py
@@ -1,41 +1,5 @@
 "GeoIP related code"
 
-#request.registry.settings.get
-
-#>>> import geoip2.database
-#>>>
-#>>> # This creates a Reader object. You should use the same object
-#>>> # across multiple requests as creation of it is expensive.
-#>>> reader = geoip2.database.Reader('/path/to/GeoLite2-City.mmdb')
-#>>>
-#>>> # Replace "city" with the method corresponding to the database
-#>>> # that you are using, e.g., "country".
-#>>> response = reader.city('128.101.101.101')
-#>>>
-#>>> response.country.iso_code
-#'US'
-#>>> response.country.name
-#'United States'
-#>>> response.country.names['zh-CN']
-#u'美国'
-#>>>
-#>>> response.subdivisions.most_specific.name
-#'Minnesota'
-#>>> response.subdivisions.most_specific.iso_code
-#'MN'
-#>>>
-#>>> response.city.name
-#'Minneapolis'
-#>>>
-#>>> response.postal.code
-#'55455'
-#>>>
-#>>> response.location.latitude
-#44.9733
-#>>> response.location.longitude
-#-93.2323
-#>>> reader.close()
-
 import geoip2.database
 from collections import OrderedDict
 
